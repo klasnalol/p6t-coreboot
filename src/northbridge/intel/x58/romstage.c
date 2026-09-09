7b7b9cddc1f5911a3ed589e2df92157af8b38c20
klasnalol <kapklas@gmail.com>
2026-09-10 03:18:48 +0500

    asus/p6t_se: add speaker bring-up diagnostics

--- src/northbridge/intel/x58/romstage.c
+++ src/northbridge/intel/x58/romstage.c
@@ -5,10 +5,18 @@
 #include <halt.h>
 #include <post.h>
 
+#include "../../../mainboard/asus/p6t_se/beep.h"
+
 void mainboard_romstage_entry(void)
 {
 	post_code(0xe0);
-	printk(BIOS_EMERG, "P6T SE/X58: reached romstage with CAR alive\n");
+	p6t_se_beep(3);
+
+	printk(BIOS_EMERG,
+	       "P6T SE/X58: reached romstage with CAR alive\n");
+
 	post_code(0xe1);
+	p6t_se_beep(4);
+
 	die("P6T SE/X58: DRAM initialization is not implemented yet\n");
 }
